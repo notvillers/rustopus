822f8baaaea8e42f4194dda69a6d3aab26d5f351
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2025-07-01 10:11:22 +0200

    test route #2

--- src/routes/test.rs
+++ src/routes/test.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, Responder};
 use crate::routes::default::send_xml;
 
-#[get("/get-stocks")]
+#[get("/get-test")]
 async fn get_test() -> impl Responder {
     send_xml("<Envelope>OK</Envelope>".to_string())
 }
