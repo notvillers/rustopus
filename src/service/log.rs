a68cc93a6ccdb3a5fec8160d15d7df5258708d24
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2025-07-30 13:38:18 +0200

    remove ```./.vscode```

--- src/service/log.rs
+++ src/service/log.rs
@@ -60,7 +60,6 @@ enum LogType {
 
 
 fn log_handler<S: AsRef<str>>(message: S, log_type: Option<LogType>) {
-
     let error_prefix = match log_type.as_ref().unwrap_or(&LogType::Ok) {
         LogType::Error => "ERROR: ".to_string(),
         _ => "".to_string()
