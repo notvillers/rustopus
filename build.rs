ed6952d3e56c32ecff69b52928b2f90067662562
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2025-08-21 14:23:55 +0200

    c replace
    + new build method

--- build.rs
+++ build.rs
@@ -1,8 +1,27 @@
 use std::path::PathBuf;
 
-fn main() {
-    let append_c = PathBuf::from("src").join("service").join("append.c");
+pub struct BuildableCFile {
+    pub path: PathBuf,
+    pub name: String
+}
+
+impl From<(PathBuf, &str)> for BuildableCFile {
+    fn from((path, name): (PathBuf, &str)) -> Self {
+        BuildableCFile {
+            path: path,
+            name: name.to_string()
+        }
+    }
+}
+
+
+fn build_c_file(buildabe_c_file: BuildableCFile) {
     cc::Build::new()
-        .file(append_c)
-        .compile("append");
+        .file(buildabe_c_file.path)
+        .compile(&buildabe_c_file.name);
+}
+
+
+fn main() {
+    build_c_file((PathBuf::from("src").join("C").join("append.c"), "append").into());
 }
