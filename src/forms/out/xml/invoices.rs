7027d2fdd8d8fe9b3c24e32f619b9c17b1c21aff
Villers <117743062+notvillers@users.noreply.github.com>
2026-06-24 16:08:01 +0200

    out invoice macroed

--- src/forms/out/xml/invoices.rs
+++ src/forms/out/xml/invoices.rs
@@ -1,17 +1,90 @@
 // Barcodes english struct(s) for XML(s) got from the Octopus call
 use chrono::NaiveDate;
-use serde::Serialize;
 use quick_xml;
 
-use crate::forms::{
-    r#in::xml::invoices as o8_invoices,
-    out::xml::defaults as p_defaults
+use crate::{
+    macros::out::OutModelDeriveOnly,
+    forms::{
+        r#in::xml::invoices as o8_invoices,
+        out::xml::defaults as p_defaults
+    }
 };
 
-#[derive(Serialize)]
-pub struct Envelope {
-    pub body: Body
+OutModelDeriveOnly! {
+    pub struct Envelope {
+        pub body: Body
+    }
+
+    pub struct Body {
+        pub response: Response
+    }
+
+    pub struct Response {
+        pub result: Result
+    }
+    
+    pub struct Result {
+        pub answer: Answer
+    }
+
+    pub struct Answer {
+        pub version: String,
+        pub invoices: Invoices,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub error: Option<p_defaults::Error>
+    }
+
+    pub struct Invoices {
+        pub invoice: Vec<Invoice>,
+    }
+
+    pub struct Invoice {
+        pub head: Head,
+        pub products: Products
+    }
+
+    pub struct Head {
+        pub id: i64,
+        pub no: Option<String>,
+        pub date: Option<NaiveDate>,
+        pub completition_date: Option<NaiveDate>,
+        pub payment_deadline: Option<NaiveDate>,
+        pub net_price: Option<f64>,
+        pub price: Option<f64>,
+        pub remaining: Option<f64>,
+        pub cancellation_no: Option<String>,
+        pub currency: String,
+        pub pid: i64,
+        pub partner_name: String,
+        pub status: i64,
+        pub foreign_order_no: Option<String>,
+        pub delivery_name: Option<String>,
+        pub delivery_country: Option<String>,
+        pub delivery_zip: Option<String>,
+        pub delivery_city: Option<String>,
+        pub delivery_street: Option<String>
+    }
+
+    pub struct Products {
+        pub product: Vec<Product>
+    }
+
+    pub struct Product {
+        pub lot_no: u64,
+        pub id: u64,
+        pub no: String,
+        pub name: String,
+        pub qty: Option<f64>,
+        pub unit: String,
+        pub net_unit_price: Option<f64>,
+        pub unit_price: Option<f64>,
+        pub net_price: Option<f64>,
+        pub price: Option<f64>,
+        pub order_no: Option<String>,
+        pub order_foreign_no: Option<String>
+    }
 }
+
 
 impl From<o8_invoices::Envelope> for Envelope {
     fn from(e: o8_invoices::Envelope) -> Self {
@@ -22,11 +95,6 @@ impl From<o8_invoices::Envelope> for Envelope {
 }
 
 
-#[derive(Serialize)]
-pub struct Body {
-    pub response: Response
-}
-
 impl From<o8_invoices::Body> for Body {
     fn from(b: o8_invoices::Body) -> Self {
         Self {
@@ -35,11 +103,6 @@ impl From<o8_invoices::Body> for Body {
     }
 }
 
-
-#[derive(Serialize)]
-pub struct Response {
-    pub result: Result
-}
 
 impl From<o8_invoices::GetSzamlakAuthResponse> for Response {
     fn from(r: o8_invoices::GetSzamlakAuthResponse) -> Self {
@@ -50,11 +113,6 @@ impl From<o8_invoices::GetSzamlakAuthResponse> for Response {
 }
 
 
-#[derive(Serialize)]
-pub struct Result {
-    pub answer: Answer
-}
-
 impl From<o8_invoices::GetSzamlakAuthResult> for Result {
     fn from(r: o8_invoices::GetSzamlakAuthResult) -> Self {
         Self {
@@ -63,14 +121,6 @@ impl From<o8_invoices::GetSzamlakAuthResult> for Result {
     }
 }
 
-
-#[derive(Serialize)]
-pub struct Answer {
-    pub version: String,
-    pub invoices: Invoices,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub error: Option<p_defaults::Error>
-}
 
 impl From<o8_invoices::Valasz> for Answer {
     fn from(v: o8_invoices::Valasz) -> Self {
@@ -83,11 +133,6 @@ impl From<o8_invoices::Valasz> for Answer {
 }
 
 
-#[derive(Serialize)]
-pub struct Invoices {
-    pub invoice: Vec<Invoice>,
-}
-
 impl From<o8_invoices::Szamlak> for Invoices {
     fn from(sz: o8_invoices::Szamlak) -> Self {
         Self {
@@ -96,12 +141,6 @@ impl From<o8_invoices::Szamlak> for Invoices {
     }
 }
 
-
-#[derive(Serialize)]
-pub struct Invoice {
-    pub head: Head,
-    pub products: Products
-}
 
 impl From<o8_invoices::Szamla> for Invoice {
     fn from(sz: o8_invoices::Szamla) -> Self {
@@ -112,29 +151,6 @@ impl From<o8_invoices::Szamla> for Invoice {
     }
 }
 
-
-#[derive(Serialize)]
-pub struct Head {
-    pub id: i64,
-    pub no: Option<String>,
-    pub date: Option<NaiveDate>,
-    pub completition_date: Option<NaiveDate>,
-    pub payment_deadline: Option<NaiveDate>,
-    pub net_price: Option<f64>,
-    pub price: Option<f64>,
-    pub remaining: Option<f64>,
-    pub cancellation_no: Option<String>,
-    pub currency: String,
-    pub pid: i64,
-    pub partner_name: String,
-    pub status: i64,
-    pub foreign_order_no: Option<String>,
-    pub delivery_name: Option<String>,
-    pub delivery_country: Option<String>,
-    pub delivery_zip: Option<String>,
-    pub delivery_city: Option<String>,
-    pub delivery_street: Option<String>
-}
 
 impl From<o8_invoices::Fej> for Head {
     fn from(f: o8_invoices::Fej) -> Self {
@@ -163,11 +179,6 @@ impl From<o8_invoices::Fej> for Head {
 }
 
 
-#[derive(Serialize)]
-pub struct Products {
-    pub product: Vec<Product>
-}
-
 impl From<Vec<o8_invoices::Tetel>> for Products {
     fn from(t: Vec<o8_invoices::Tetel>) -> Self {
         Self {
@@ -176,22 +187,6 @@ impl From<Vec<o8_invoices::Tetel>> for Products {
     }
 }
 
-
-#[derive(Serialize)]
-pub struct Product {
-    pub lot_no: u64,
-    pub id: u64,
-    pub no: String,
-    pub name: String,
-    pub qty: Option<f64>,
-    pub unit: String,
-    pub net_unit_price: Option<f64>,
-    pub unit_price: Option<f64>,
-    pub net_price: Option<f64>,
-    pub price: Option<f64>,
-    pub order_no: Option<String>,
-    pub order_foreign_no: Option<String>
-}
 
 impl From<o8_invoices::Tetel> for Product {
     fn from(t: o8_invoices::Tetel) -> Self {
