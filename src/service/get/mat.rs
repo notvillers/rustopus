772cb9618afba3a5e4afb6fe3eadd657401f4fef
Villers <117743062+notvillers@users.noreply.github.com>
2026-07-01 15:44:19 +0200

    Update mat.rs

--- src/service/get/mat.rs
+++ src/service/get/mat.rs
@@ -51,9 +51,9 @@ pub async fn get_mat(call_data: CallData) -> MatData {
     return match quick_xml::de::from_str::<o8_mat::Envelope>(&response) {
         Ok(envelope) => {
             match (call_data.is_csv(), call_data.is_hu()) {
-                (true, _) => return MatData::CSV(MatCSV::En(envelope.into())),
-                (_, true) => return MatData::XML(MatXML::Hu(envelope)),
-                _ => return MatData::XML(MatXML::En(envelope.into()))
+                (true, _) => MatData::CSV(MatCSV::En(envelope.into())),
+                (_, true) => MatData::XML(MatXML::Hu(envelope)),
+                _ => MatData::XML(MatXML::En(envelope.into()))
             }
         },
         Err(error) => {
